d1f2ee22d8a713d7f75e5879bb1c0ce395d0d404
Dave Albert <dave.albert@publicisdhealthcare.ie>
2013-03-27 07:57:39 +0000

    Initial Xcode project

---
+++ Take Control/Take Control/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Take Control
+//
+//  Created by Dave Albert on 27/03/2013.
+//  Copyright (c) 2013 Moodocity Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Take Control/Take Control/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Take Control
+//
+//  Created by Dave Albert on 27/03/2013.
+//  Copyright (c) 2013 Moodocity Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
