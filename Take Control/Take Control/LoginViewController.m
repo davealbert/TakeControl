c1991b029280e07dc46cd3822b25aa83ce3e4ac9
Dave Albert <dave.albert@publicisdhealthcare.ie>
2013-03-28 07:48:21 +0000

    ViewController  --> LoginViewController

--- Take Control/Take Control/LoginViewController.m
+++ Take Control/Take Control/LoginViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013 Moodocity Ltd. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad
 {
